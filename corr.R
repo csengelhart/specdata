6aec82c5011effefa205346298f3f8e0c1b8bed0
csengelhart <62675909+csengelhart@users.noreply.github.com>
2020-06-08 03:50:40 -0500

    Update corr.R
    removed else statement, added result and worked around no else

--- corr.R
+++ corr.R
@@ -1,22 +1,21 @@
 corr <- function(directory,threshold = 0){
         list_file <- list.files(directory, pattern = ".csv",full.names = T)
-        cor_empty <- c()
+        result <-vector(mode="numeric", length=0)
         id = 1:332
+        
         
         for (i in id){
                 datas <- read.csv(list_file[i])
                 num_comp_cases <- nrow(na.omit(datas))
                 if(num_comp_cases > threshold){
                         cor_data <- cor(datas$sulfate,datas$nitrate,use = "complete.obs")
-                        cor_empty <- c(cor_empty,cor_data)
+                        result <- c(result,cor_data)
                 }
-                else
-                        cor_empty <- numeric()
-                        
-                }
-                cor_empty
-        
+                
         }
+        result
         
+}
+
 
 
